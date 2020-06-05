28e1d8725c3bf4caf35cf2b42613058909a70e84
Marie <marie.monier@ecl19.ec-lyon.fr>
2020-06-05 19:30:35 +0200

    ajout de get_monnaie et get_coords

--- Creation_base_donnees.py
+++ Creation_base_donnees.py
@@ -103,10 +103,90 @@ def get_population(country):
     print('Impossible de trouver le nombre d\'habitants')
     return None
 
+def get_monnaie(country):
+    info=get_info(country)
+    if "currency" in info:
+        currency=info['currency'].replace('\n',' ')
+        m = re.match(".*?\[\[([\w\s',(.)|-]+)\]\]",currency)
+        currency=m.group(1)
+        currency=currency.split('|')
+        return currency[0]
+    
+    # En cas d'échec
+    print('Impossible de trouver la monnaie')
+    return None
 
+def get_coords(country):
+    info=get_info(country)
+    if 'coordinates' in info:
+        coord=info['coordinates']
+        m=re.match('(?i).*{{coord\s*\|([^}]*)}}',coord)
+        
+        if m == None:
+            print('Impossible d\'afficher proprement les coordonnées :'+coord)
+            return None
+        coord=m.group(1)
+        if coord[0:1] in '0123456789':
+            return cv_coords(coord)
+    
+    # En cas d'échec
+    print('Impossible de trouver les coordonnées')
+    return None
 
+def cv_coords(str_coords):
+    # on découpe au niveau des "|" 
+    c = str_coords.split('|')
 
+    # on extrait la latitude en tenant compte des divers formats
+    lat = float(c.pop(0))
+    if (c[0] == 'N'):
+        c.pop(0)
+    elif ( c[0] == 'S' ):
+        lat = -lat
+        c.pop(0)
+    elif ( len(c) > 1 and c[1] == 'N' ):
+        lat += float(c.pop(0))/60
+        c.pop(0)
+    elif ( len(c) > 1 and c[1] == 'S' ):
+        lat += float(c.pop(0))/60
+        lat = -lat
+        c.pop(0)
+    elif ( len(c) > 2 and c[2] == 'N' ):
+        lat += float(c.pop(0))/60
+        lat += float(c.pop(0))/3600
+        c.pop(0)
+    elif ( len(c) > 2 and c[2] == 'S' ):
+        lat += float(c.pop(0))/60
+        lat += float(c.pop(0))/3600
+        lat = -lat
+        c.pop(0)
 
+    # on fait de même avec la longitude
+    lon = float(c.pop(0))
+    if (c[0] == 'W'):
+        lon = -lon
+        c.pop(0)
+    elif ( c[0] == 'E' ):
+        c.pop(0)
+    elif ( len(c) > 1 and c[1] == 'W' ):
+        lon += float(c.pop(0))/60
+        lon = -lon
+        c.pop(0)
+    elif ( len(c) > 1 and c[1] == 'E' ):
+        lon += float(c.pop(0))/60
+        c.pop(0)
+    elif ( len(c) > 2 and c[2] == 'W' ):
+        lon += float(c.pop(0))/60
+        lon += float(c.pop(0))/3600
+        lon = -lon
+        c.pop(0)
+    elif ( len(c) > 2 and c[2] == 'E' ):
+        lon += float(c.pop(0))/60
+        lon += float(c.pop(0))/3600
+        c.pop(0)
+    
+    # on renvoie un dictionnaire avec les deux valeurs
+    return {'lat':lat, 'lon':lon }
 
 
 
