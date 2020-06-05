72aeb993ca8fdbbf7fce92625b6e72d52fb9ef52
Marie <marie.monier@ecl19.ec-lyon.fr>
2020-06-05 17:33:28 +0200

    ajout_get_leader

--- Creation_base_donnees.py
+++ Creation_base_donnees.py
@@ -1,33 +1,12 @@
-# import du module d'accès à la base de données
+import wptools
 import sqlite3
-# Ouverture d'une connexion avec la base de données
 from zipfile import ZipFile
 import json
 
 def get_info(country):
     with ZipFile('{}.zip'.format('europe'),'r') as z:
-    
         # infobox du pays
         return json.loads(z.read('{}.json'.format(country)))
-
-conn = sqlite3.connect('base_donnees.sqlite')
-
-def add_country(conn,country):
-    # préparation de la commande SQL
-    c = conn.cursor()
-    sql = 'INSERT OR REPLACE INTO countries VALUES (?, ?, ?, ?, ?)'
-    info=get_info(country)
-
-    # les infos à enregistrer
-    name = get_name(info)
-    capital = get_capital(info)
-    coords = get_coords(info)
-
-    # soumission de la commande (noter que le second argument est un tuple)
-    c.execute(sql,(country, name, capital, coords['lat'],coords['lon']))
-    conn.commit()
-
-# Récupération du nom complet du pays
 
 def get_name(country):
     info=get_info(country)
@@ -46,8 +25,6 @@ def get_name(country):
     print('Le nom du pays n\'a pas été trouvé')
     return None
 
-# Récupération de la capitale du pays
-
 def get_capital(country):
     info=get_info(country)
     if 'capital' in info:
@@ -62,6 +39,32 @@ def get_capital(country):
     
     # En cas d'échec
     print('Impossible de trouver la capitale')
+    return None
+
+def get_leader_titre(country):
+    info=get_info(country)
+    if 'leader_title1' in info:
+        leader_titre=info['leader_title1'].replace('\n',' ')
+        m = re.match(".*?\[\[([\w\s',(.)|-]+)\]\]",leader_titre)
+        leader_titre=m.group(1)
+        
+        return leader_titre
+    
+    # En cas d'échec
+    print('Impossible de trouver le leader')
+    return None
+
+def get_leader_name(country):
+    info=get_info(country)
+    if 'leader_name1' in info:
+        leader_name=info['leader_name1'].replace('\n',' ')
+        m = re.match(".*?\[\[([\w\s',(.)|-]+)\]\]",leader_name)
+        leader_name=m.group(1)
+        
+        return leader_name
+    
+    # En cas d'échec
+    print('Impossible de trouver le leader')
     return None
 
 
@@ -79,6 +82,20 @@ def get_capital(country):
 
 
 
-
-
-
+# 
+# conn = sqlite3.connect('base_donnees.sqlite')
+# 
+# def add_country(conn,country):
+#     # préparation de la commande SQL
+#     c = conn.cursor()
+#     sql = 'INSERT OR REPLACE INTO countries VALUES (?, ?, ?, ?, ?)'
+#     info=get_info(country)
+# 
+#     # les infos à enregistrer
+#     name = get_name(info)
+#     capital = get_capital(info)
+#     coords = get_coords(info)
+# 
+#     # soumission de la commande (noter que le second argument est un tuple)
+#     c.execute(sql,(country, name, capital, coords['lat'],coords['lon']))
+#     conn.commit()
